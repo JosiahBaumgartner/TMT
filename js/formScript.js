68be4a39b00bbd9d29a5d8d7b219154b386f994c
Josiah Baumgartner <48170542+JosiahBaumgartner@users.noreply.github.com>
2020-04-28 12:12:01 -0700

    More Form stuff, mobile breakpoints
    also some styling babbie things. also new header text on services page.

--- js/formScript.js
+++ js/formScript.js
@@ -30,6 +30,7 @@ function nextStep() {
     header1.innerHTML = name + ", if you’re in a hurry we’ll just take a phone number and/or an email at this point,<br>but it would be nice if you could tell us what’s going on. (Just 4 more questions)";
     show(phoneNumberInput);
     show(emailAddressInput);
+    showInline(submitButton);
   } else if ( (step === 2 && emailRegex.test(emailAddressInput.value)) || (step === 2 && phoneRegex.test(phoneNumberInput.value)) ) {
     step = 3;
     header1.innerHTML = "We can help you learn lots of different things!<br>What subject(s) would you like tutoring in?";
@@ -40,18 +41,18 @@ function nextStep() {
     step++;
     header1.innerHTML = "When are you available for tutoring?<br>(Please give us several options)";
     hide(subjectInput);
-    for (let i=0; i<timeDiv.children.length; i++){
+    for (let i=1; i<timeDiv.children.length; i++){
       showInline(timeDiv.children[i].children[0]);
       showInline(timeDiv.children[i].children[0].children[0]);
     }
   } else if ( step === 4 ) {
     step++;
     header1.innerHTML = "Are you interested in online or in person tutoring?<br>If in person, approximately where would you like your sessions to be located?";
-    for (let i=0; i<timeDiv.children.length; i++){
+    for (let i=1; i<timeDiv.children.length; i++){
       hide(timeDiv.children[i]);
     }
-    for (let i=0; i<locationDiv.children.length; i++){
-      if ( i !== 2 ) {
+    for (let i=1; i<locationDiv.children.length; i++){
+      if ( locationDiv.children[i].classList.contains("w-checkbox") ) {
         show(locationDiv.children[i]);
       }
     }
@@ -60,7 +61,7 @@ function nextStep() {
   } else if (step === 5) {
     step++;
     header1.innerHTML = "Leave us anything else you’d like to tell us.<br>We’re listening to you?";
-    for (let i=0; i<locationDiv.children.length; i++){
+    for (let i=1; i<locationDiv.children.length; i++){
       hide(locationDiv.children[i]);
     }
     show(textbox);
@@ -70,11 +71,11 @@ function nextStep() {
     hide(button);
     hide(textbox);
     for (let i=0; i<allInputs.length; i++){
-      if (allInputs[i].classList.contains("time-input") !== true){
+      if (allInputs[i].classList.contains("time-input") !== true && allInputs[i].disabled === false){
         allInputs[i].style.display = "block";
       }
     }
-    for (let i=0; i<timeDiv.children.length; i++){
+    for (let i=1; i<timeDiv.children.length; i++){
       console.log(i);
       timeDiv.children[i].style.display = "inline-block";
     }
@@ -110,12 +111,26 @@ for (let i=0; i<document.querySelectorAll("label").length; i++){
 header1.innerHTML = "Thanks for reaching out to us. <br> What's your name?"
 show(header1);
 show(nameInput);
+locationDiv.children[3].disabled = true;
 document.querySelector("form").appendChild(document.createElement("button"));
 const button = document.querySelector("button");
 button.setAttribute("type", "button");
 button.classList.add("submit-button");
 button.classList.add("w-button");
 document.querySelector("button").textContent = "Next";
+
+locationDiv.children[2].addEventListener("click", () => {
+
+  if (locationDiv.children[2].children[0].checked){
+    show(locationDiv.children[3]);
+    locationDiv.children[3].disabled = false;
+  } else {
+    hide(locationDiv.children[3]);
+    locationDiv.children[3].disabled = true;
+    locationDiv.children[3].value = "";
+  }
+
+});
 
 button.addEventListener("click", () => { nextStep() });
 document.addEventListener('keypress', function (e) {
@@ -125,15 +140,25 @@ document.addEventListener('keypress', function (e) {
     }
 });
 
-for (let i=0; i<timeDiv.children.length; i++){
+for (let i=1; i<timeDiv.children.length; i++){
   timeDiv.children[i].children[0].children[0].addEventListener("click", () => {
     if(event.target.checked === true) {
       show(event.target.parentElement.parentElement.lastElementChild);
+      event.target.parentElement.parentElement.lastElementChild.disable = false;
     } else {
       event.target.parentElement.parentElement.lastElementChild.value = "";
       hide(event.target.parentElement.parentElement.lastElementChild);
+      event.target.parentElement.parentElement.lastElementChild.disable = true;
     }
   });
 }
 
-submitButton.addEventListener("click", () => { confirm() });
+submitButton.addEventListener("click", () => {
+
+  if ((nameRegex.test(nameInput.value) && emailRegex.test(emailAddressInput.value)) || (nameRegex.test(nameInput.value) && phoneRegex.test(phoneNumberInput.value))) {
+    confirm();
+  } else {
+    event.preventDefault();
+  }
+
+});
